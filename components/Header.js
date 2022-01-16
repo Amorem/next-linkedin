b4d8a29b0ab9ac6a04eda1ab8cad387cf4428aad
Amorem <dev@amorem.com>
2022-01-16 17:27:51 +0100

    WIP: main Header

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import Image from "next/image";
+import HeaderLink from "./HeaderLink";
+import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import { Avatar } from "@mui/material";
+
+function Header() {
+  return (
+    <header>
+      {/* Left */}
+      <div className="flex items-center space-x-2 w-full max-w-xs">
+        <Image
+          src="https://rb.gy/bizvqj"
+          width={45}
+          height={45}
+          alt="LinkedIn logo white"
+        />
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+          <SearchRoundedIcon />
+          <input
+            type="text"
+            placeholder="Search"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+          />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={GroupIcon} text="My Network" feed />
+        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
