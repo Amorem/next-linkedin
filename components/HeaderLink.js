9640d12f941e12d1bc64616cc2be3cc6f741e8de
Amorem <dev@amorem.com>
2022-01-19 14:53:51 +0100

    Sidebar Top

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -7,7 +7,7 @@ function HeaderLink({ Icon, text, avatar, feed, active, hidden }) {
         feed
           ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1"
           : "text-gray-500 hover:text-gray-700"
-      } ${active && "!text-black dark:text-white"}`}
+      } ${active && "!text-black dark:!text-white"}`}
     >
       {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
       <h4
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
-        <title>LinkedIn</title>
+        <title>Feed | LinkedIn</title>
         <meta name="description" content="LinkedIn" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* Sidebar */}
+          <Sidebar />
+          {/* Feed */}
+        </div>
+        {/* Widget */}
+      </main>
     </div>
   );
 }
